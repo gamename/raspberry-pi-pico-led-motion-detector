4d61ba10454b505a5a46ddf9f274db80b11b159d
gamename <tennis.n.smith@gmail.com>
2022-08-10 16:22:52 -0500

    update uPython version with code changes to accommodate

--- boot.py
+++ boot.py
@@ -1,26 +1,25 @@
 import time
-from neopixel import Neopixel
+from neopixel import NeoPixel
 from machine import Pin
 
-PIXEL_COUNT = 30
+PIXEL_COUNT = 55
 LIGHT_MINUTES = 3
 
-pixels = Neopixel(PIXEL_COUNT, 0, 0, "GRB")
+pixels = NeoPixel(Pin(0), PIXEL_COUNT)
 pir = Pin(1, Pin.IN, Pin.PULL_DOWN)
 
 WHITE = (255, 255, 255)
 OFF = (0, 0, 0)
 SLEEP_TIME = 60 * LIGHT_MINUTES
 
-pixels.brightness(100)
 pixels.fill(OFF)
-pixels.show()
+pixels.write()
 
 while True:
     if pir.value():
         pixels.fill(WHITE)
-        pixels.show()
+        pixels.write()
         time.sleep(SLEEP_TIME)
         pixels.fill(OFF)
-        pixels.show()
+        pixels.write()
     time.sleep(1)
