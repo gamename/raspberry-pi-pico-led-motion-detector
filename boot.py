8b1680f6e88fa5661e408e0487685c094bc375f9
gamename <tennis.n.smith@gmail.com>
2022-08-28 04:49:37 -0500

    documented and simplified

--- boot.py
+++ boot.py
@@ -1,27 +1,54 @@
+'''
+This is a simple example of how to light a strip of ws2812b pixels with the Raspberry Pi Pico using
+a motion detector
+'''
 import time
 from neopixel import NeoPixel
 from machine import Pin
 
+# How many pixels do we have?
 PIXEL_COUNT = 16
+
+# How long should we stay
 LIGHT_MINUTES = 3
+
+# Which pin controls the light strip?
 PIXEL_PIN = Pin(0)
+
+# Which pin controls the motion detector?
 MOTION_DETECTION_PIN = 1
+
+# Define the color we want to use for the light strip
+WHITE = (255, 255, 255)
+
+# Define how we want to turn off the light strip
+OFF = (0, 0, 0)
+
+# How long should we stay on after detecting motion?
+SHINE_TIME = 60 * LIGHT_MINUTES
+
+
+def set_color(strip, color):
+    '''
+    Set the color of the pixel strip
+    Args:
+        strip (): The pixel strip name
+        color (): The color to set
+
+    Returns:
+        Nothing
+    '''
+    strip.fill(color)
+    strip.write()
+
 
 pixels = NeoPixel(PIXEL_PIN, PIXEL_COUNT)
 motion = Pin(MOTION_DETECTION_PIN, Pin.IN, Pin.PULL_DOWN)
 
-WHITE = (255, 255, 255)
-OFF = (0, 0, 0)
-SHINE_TIME = 60 * LIGHT_MINUTES
-
-pixels.fill(OFF)
-pixels.write()
+set_color(pixels, OFF)
 
 while True:
     if motion.value():
-        pixels.fill(WHITE)
-        pixels.write()
+        set_color(pixels, WHITE)
         time.sleep(SHINE_TIME)
-        pixels.fill(OFF)
-        pixels.write()
-    time.sleep(1)
+        set_color(pixels, OFF)
