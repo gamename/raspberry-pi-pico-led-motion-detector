0a6d2581b7f1deb6d2b9aadaefa4b413816aa3da
gamename <tennis.n.smith@gmail.com>
2022-08-28 04:21:45 -0500

    set pixel number. renamed vars to be more understandable

--- boot.py
+++ boot.py
@@ -2,24 +2,26 @@ import time
 from neopixel import NeoPixel
 from machine import Pin
 
-PIXEL_COUNT = 55
+PIXEL_COUNT = 16
 LIGHT_MINUTES = 3
+PIXEL_PIN = Pin(0)
+MOTION_DETECTION_PIN = 1
 
-pixels = NeoPixel(Pin(0), PIXEL_COUNT)
-pir = Pin(1, Pin.IN, Pin.PULL_DOWN)
+pixels = NeoPixel(PIXEL_PIN, PIXEL_COUNT)
+motion = Pin(MOTION_DETECTION_PIN, Pin.IN, Pin.PULL_DOWN)
 
 WHITE = (255, 255, 255)
 OFF = (0, 0, 0)
-SLEEP_TIME = 60 * LIGHT_MINUTES
+SHINE_TIME = 60 * LIGHT_MINUTES
 
 pixels.fill(OFF)
 pixels.write()
 
 while True:
-    if pir.value():
+    if motion.value():
         pixels.fill(WHITE)
         pixels.write()
-        time.sleep(SLEEP_TIME)
+        time.sleep(SHINE_TIME)
         pixels.fill(OFF)
         pixels.write()
     time.sleep(1)
